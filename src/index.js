f157658f881402ce01b0c7542da14be7c1fb9dda
Mariy Rodina <marusea_rod@list.ru>
2021-11-22 12:21:57 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//styles
+import './styles/normalize.scss'
+import './styles/font.scss'
+import './styles/variables.scss'
+import './styles/grid.scss'
+import './styles/common.scss'
+
+//styles components
+
+import './styles/components/buttons/buttons.scss'
+import './styles/components/main_banner/main_banner.scss'
+import './styles/components/socs/socs.scss'
+import './styles/components/faq/faq.scss'
+import './styles/components/carousel/carousel.scss'
+import './styles/components/faq_block/faq_block.scss'
+import './styles/components/reference/reference.scss'
+import './styles/components/faq_page/faq_page.scss'
+
+
+//scripts
+import './scripts/carousel'
